79d0b9a9903ebfd929ae3f46a024730ba578c6f3
kunal <kunalgk1999@gmail.com>
2020-05-03 10:27:33 +0530

    updated homepage

--- src/components/Room/JoinLink.js
+++ src/components/Room/JoinLink.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { homepage } from '../../../package.json';
 
 import hubStore from '../../stores/hubStore';
 
@@ -22,7 +23,7 @@ export default function Room() {
         <span>
           connect at{' '}
           <b>
-            https://p2p-conf.netlify.app/follow/
+            {homepage}follow/
             {peerId}
           </b>
         </span>
