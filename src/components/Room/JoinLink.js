cec3842724d79ffb55748b385872e11f7efd6c10
kunal <kunalgk1999@gmail.com>
2020-05-03 14:29:29 +0530

    updated join link with copy option

--- src/components/Room/JoinLink.js
+++ src/components/Room/JoinLink.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import copy from 'copy-to-clipboard';
 import { homepage } from '../../../package.json';
 
 import hubStore from '../../stores/hubStore';
 
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { Box } from '@material-ui/core';
 
-export default function Room() {
+export default function JoinLink() {
   const [peerId, setPeerId] = useState(null);
+  const joinUrl = homepage + 'follow/' + peerId;
 
   useEffect(() => {
     hubStore.addChangeListener(() => {
@@ -18,18 +24,44 @@ export default function Room() {
   }, []);
 
   return (
-    <Typography variant="subtitle1" style={{ padding: 12 }}>
+    <Paper
+      style={{
+        width: '50%',
+        overflow: 'hidden',
+        marginBottom: 12,
+        position: 'relative',
+      }}
+    >
       {peerId ? (
-        <span>
-          connect at{' '}
-          <b>
-            {homepage}follow/
-            {peerId}
-          </b>
-        </span>
+        <Box>
+          <Typography
+            variant="subtitle1"
+            style={{
+              padding: 12,
+              width: 'calc(100% - 30px)',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            share link
+            <i> {joinUrl}</i>
+          </Typography>
+          <IconButton
+            aria-label="copy"
+            onClick={() => {
+              copy(joinUrl);
+            }}
+            style={{ position: 'absolute', right: 0, top: 0, bottom: 0 }}
+          >
+            <FileCopyIcon />
+          </IconButton>
+        </Box>
       ) : (
-        <CircularProgress />
+        <Box style={{ padding: 12 }}>
+          <CircularProgress />
+        </Box>
       )}
-    </Typography>
+    </Paper>
   );
 }
